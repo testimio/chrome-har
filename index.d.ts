f165ddb1fee0dd83c78ad6e6782866e107123977
Bnaya Peretz <me@bnaya.net>
2020-07-28 18:02:14 +0300

    Iteration

--- index.d.ts
+++ index.d.ts
@@ -5,21 +5,23 @@ import {
 // Possible events, from the switch case inside chrome-har code
 // We can expand if to all kind of events easily
 declare const possibleEvents: [
-    'Page.frameStartedLoading',
-    'Page.frameScheduledNavigation',
-    'Page.navigatedWithinDocument',
-    'Network.requestWillBeSent',
-    'Network.requestServedFromCache',
-    'Network.responseReceived',
     'Network.dataReceived',
+    'Network.loadingFailed',
     'Network.loadingFinished',
-    'Page.loadEventFired',
+    'Network.requestServedFromCache',
+    'Network.requestWillBeSent',
+    'Network.resourceChangedPriority',
+    'Network.responseReceived',
     'Page.domContentEventFired',
     'Page.frameAttached',
-    'Network.loadingFailed',
-    'Network.resourceChangedPriority',
+    'Page.frameScheduledNavigation',
+    'Page.frameStartedLoading',
+    'Page.loadEventFired',
+    'Page.navigatedWithinDocument',
 ];
 
+// this will make it take all events
+// export type ChromeHarMessage = DevtoolsProtocolEventMap[keyof DevtoolsProtocolEventMap];
 export type ChromeHarMessage = DevtoolsProtocolEventMap[typeof possibleEvents[number]];
 
 export type HarFromMessages = (
